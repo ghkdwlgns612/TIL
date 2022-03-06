f211d44b78865b39763b8df79130f61799eb57f8
ghkdwlgns612 <ghkdwlgns612@naver.com>
2022-03-06 10:49:37 +0900

    QueryDsl에 대하여

---
+++ JPA/QueryDslBasic/src/main/java/com/example/querydslbasic/QuerydslbasicApplication.java
@@ -0,0 +1,11 @@
+package com.example.querydslbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslbasicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslbasicApplication.class, args);
+    }
+}
---
+++ JPA/QueryDslBasic/src/test/java/com/example/querydslbasic/QuerydslbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydslbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslbasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

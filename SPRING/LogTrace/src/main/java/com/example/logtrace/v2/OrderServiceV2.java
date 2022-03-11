b94195f444af99c9fed87185ff26a195a14ebf33
ghkdwlgns612 <ghkdwlgns612@naver.com>
2022-03-11 15:20:35 +0900

    LogTracer의 문제점에 관하여

---
+++ SPRING/LogTrace/src/main/java/com/example/logtrace/v2/OrderServiceV2.java
@@ -0,0 +1,27 @@
+package com.example.logtrace.v2;
+
+import com.example.logtrace.trace.HelloTraceV2;
+import com.example.logtrace.trace.TraceId;
+import com.example.logtrace.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepositoryV2;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepositoryV2.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
---
+++ SPRING/LogTrace/src/test/java/com/example/logtrace/LogTraceApplicationTests.java
@@ -0,0 +1,42 @@
+package com.example.logtrace;
+
+import com.example.logtrace.trace.HelloTraceV1;
+import com.example.logtrace.trace.HelloTraceV2;
+import com.example.logtrace.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogTraceApplicationTests {
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+    @Test
+    void 여begin_end_level2() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
